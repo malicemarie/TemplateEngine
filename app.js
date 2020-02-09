72c127cbec1a99d704638790fffd676c34603add
Marie Lenac <marielenac@Maries-MacBook-Pro.local>
2020-02-09 15:32:13 -0800

    changed the document title

--- app.js
+++ app.js
@@ -338,7 +338,7 @@ async function createHTML(managers, interns, engineers) {
        <meta charset="UTF-8" />
        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-       <title>Main</title>
+       <title>Team Members</title>
        <link
          rel="stylesheet"
          href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
